96d677edee35ebb9790d87dbd6bab6934d376f1a
Hossain Jubair <s2300099@BC-C02GR0BMQ6LRs-MacBook-Air.local>
2023-01-17 13:00:37 +0200

    task2 solutions

---
+++ task2.js
@@ -0,0 +1,29 @@
+//Evaluate the below:
+3 + 65 // 365
+3 + "65" // 365
+7 - "3" // 4
+15 % 5 // 0
+5 % 15 // 5
+"Java" + "Script" // JavaScript
+"Java" - "Script" // NaN
+" " + " " // "  "
+" " + 0 // " 0"
+true + true // 1+1 = 2
+true + false // 1+0 = 1
+false + true // 0+1 = 1
+false - true // 0-1= -1
+3 - 4 // -1
+
+//Evaluate the below comparisons:
+4 >= 2 // true
+6 === 2 // false
+3 <= 1 // false
+1 != 1 // false
+"A" > "B" // false
+"B" < "C" // false -> true
+"a" > "A" // true
+"b" < "A" // false
+true === false // false
+true != true // false
+
+"Go To Helsinki Business College, they said. 'It'll Will Be ´Fun´,' they said."
